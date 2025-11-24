0ce76b88eb7c85438c9c0d5d1568951513572cc2
n <nanovonlepel@gmail.com>
2025-11-24 02:36:34 -0300

    Practicando modificar cosas

--- hello.py
+++ hello.py
@@ -1,4 +1,7 @@
 print("Hola Mundo")
+#Para clonar un repositorio 
+'''abrir la terminal y: git clone (aca lo que esta en el apartado ssh de git hub)'''
+
 #comandos mas usados
 # git add
 # git status

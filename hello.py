3e1a03cc2147c5a6290a079c9683a0a8dfb961bf
n <nanovonlepel@gmail.com>
2025-11-24 02:41:15 -0300

    Practicando modificar cosas

--- hello.py
+++ hello.py
@@ -3,7 +3,20 @@ print("Hola Mundo")
 '''abrir la terminal y: git clone (aca lo que esta en el apartado ssh de git hub)'''
 
 #comandos mas usados
-# git add
-# git status
+# git add (nombre del archivo)
+# git status (para que muestre los archivos que van a ser modificados)
 # git commit -m
 # git push
+
+
+'''la primera vez pregunta quien eres
+Ejecuta
+
+  git config --global user.email "you@example.com"
+  git config --global user.name "Tu Nombre"
+
+para configurar la identidad por defecto de tu cuenta.
+Omite --global para configurar tu identidad solo en este repositorio.
+'''
+
+print("Primera carga realizada")
